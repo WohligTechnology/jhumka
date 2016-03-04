cbaa07ad5fa3dfd481b8dfbebf23528abdd84d8a
mahesh maurya <mahesh@wohlig.com>
2016-03-04 12:40:57 +0530

    html home page done

--- js/controllers.js
+++ js/controllers.js
@@ -16,6 +16,19 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
     'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
     'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
   ];
+  $scope.press=[{
+    img:"img/p1.jpg",
+    title:"The Diamond Store shortlisted for UK Blog Awards 2016",
+    desc:"It’s just been announced that a new jewellery blog, The Diamond Store Magazine, is up..."
+  },{
+    img:"img/p2.jpg",
+    title:"Selfridges unveils its christmas window",
+    desc:"It’s just been announced that a new jewellery blog, The Diamond Store Magazine, is up..."
+  },{
+    img:"img/p3.jpg",
+    title:"Designer & Antique Jewels at Dreweatts",
+    desc:"It’s just been announced that a new jewellery blog, The Diamond Store Magazine, is up..."
+  }]
 })
 
 .controller('headerctrl', function($scope, TemplateService) {
